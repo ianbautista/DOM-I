3baa2b5b1305a93380de175afa8bed7f7df40de8
ianbautista <ianfbautista@gmail.com>
2020-07-03 16:36:38 -0700

    added menu items

--- js/index.js
+++ js/index.js
@@ -51,3 +51,12 @@ headerImg.setAttribute("src", siteContent["cta"]["img-src"]);
 
 let middleImg = document.getElementById("middle-img");
 middleImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let navItems = document.querySelectorAll(".container header nav a");
+console.log(navItems);
+navItems[0].textContent = siteContent["nav"]["nav-item-1"];
+navItems[1].textContent = siteContent["nav"]["nav-item-2"];
+navItems[2].textContent = siteContent["nav"]["nav-item-3"];
+navItems[3].textContent = siteContent["nav"]["nav-item-4"];
+navItems[4].textContent = siteContent["nav"]["nav-item-5"];
+navItems[5].textContent = siteContent["nav"]["nav-item-6"];
