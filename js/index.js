248e87490b3ff8758b96ce785e46bd5fdb236d4d
ianbautista <ianfbautista@gmail.com>
2020-07-03 18:02:15 -0700

    changed nav colors to green

--- js/index.js
+++ js/index.js
@@ -63,6 +63,11 @@ navItems[3].textContent = siteContent.nav["nav-item-4"];
 navItems[4].textContent = siteContent.nav["nav-item-5"];
 navItems[5].textContent = siteContent.nav["nav-item-6"];
 
+// Changing the Nav to Green
+navItems.forEach((item) => {
+	item.style.color = "green";
+});
+
 // Header
 const h1 = document.querySelector("h1");
 h1.textContent = siteContent.cta.h1;
